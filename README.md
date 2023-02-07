9465b2b2779e3e2acad25724ce759a31c06a30d4
Piotr <p.chudalewski@gmail.com>
2023-02-07 18:34:04 +0100

    Testing CI with Netlify

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Uzupełnić
-
 # Spoon Fork App project
 
 Recipe app with custom recipe uploads.
+
+https://spoonfork.netlify.app/
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -3,7 +3,7 @@ import previewView from './previewView.js';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
-  _errorMessage = `No recipes found for your query! Please try again ;)`;
+  _errorMessage = `No recipes found for your query! Please try again ;))`;
   _message = '';
 
   _generateMarkup() {
