4028fcf5b398503da3f492484b82191b056272a8
Piotr <p.chudalewski@gmail.com>
2023-02-07 18:26:43 +0100

    config changed

--- src/js/config.js
+++ src/js/config.js
@@ -3,4 +3,3 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const API_KEY = '1b4d7e1f-9cf6-4e79-8452-95e383f997d6';
 export const MODAL_CLOSE_SEC = 2.5;
-export const TEST_BRANCH_VAR = 1;
